4755f44726bfdee495075de2d60101ff29bd7de4
SAMRAT69 <157216723+SAMRAT69@users.noreply.github.com>
2025-02-25 16:46:08 +0545

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+import CodeInput from '../components/CodeInput';
+import ResultDisplay from '../components/ResultDisplay';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <CodeInput />
+      <ResultDisplay />
+    </div>
+  );
+}
