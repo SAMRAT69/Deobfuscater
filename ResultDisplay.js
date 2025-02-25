a0a136f5cd2a86b14749cda18399eab66e1e00ab
SAMRAT69 <157216723+SAMRAT69@users.noreply.github.com>
2025-02-25 16:47:52 +0545

    Create ResultDisplay.js

---
+++ ResultDisplay.js
@@ -0,0 +1,10 @@
+const ResultDisplay = () => {
+  return (
+    <div>
+      <h2>Deobfuscated Code</h2>
+      <pre>/* Display deobfuscated code here */</pre>
+    </div>
+  );
+};
+
+export default ResultDisplay;
