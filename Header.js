a1300926201fbec5d077df3906513087187f0aa0
SAMRAT69 <157216723+SAMRAT69@users.noreply.github.com>
2025-02-25 16:46:23 +0545

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Deobfuscator</h1>
+      <p>Deobfuscate your code instantly for free!</p>
+    </header>
+  );
+};
+
+export default Header;
